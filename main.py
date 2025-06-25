fa357867874189d6ce594d824aedbbe88e1a3706
Samuel Ybarra <sybarra2076@gmail.com>
2025-06-25 08:10:28 -0500

    Giving agent ability to create conversation history with itself

--- main.py
+++ main.py
@@ -38,31 +38,39 @@ def main():
         print(f"User prompt: {user_prompt}")
 
     generate_content(client, messages, verbose)
-        
+
 
 def generate_content(client, messages, verbose):
-    response = client.models.generate_content(
-        model="gemini-2.0-flash-001",
-        contents=messages,
-        config=types.GenerateContentConfig(
-            tools=[available_functions], system_instruction=system_prompt
+    timeout = 20
+    while timeout != 0:
+        response = client.models.generate_content(
+            model="gemini-2.0-flash-001",
+            contents=messages,
+            config=types.GenerateContentConfig(
+                tools=[available_functions], system_instruction=system_prompt
+            )
         )
-    )
+        for candidate in response.candidates:
+            messages.append(candidate.content)
 
-    if verbose:
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        if verbose:
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-    if response.function_calls:
-        for function_call_part in response.function_calls:
-            try:
-                function_call_result = call_function(function_call_part, verbose)
-                if verbose:
-                    print(f"-> {function_call_result.parts[0].function_response.response}")
-            except:
-                raise Exception("Failed to match funtion to request")
-    else:
-        print(response.text)
+        if response.function_calls:
+            for function_call_part in response.function_calls:
+                try:
+                    function_call_result = call_function(function_call_part, verbose)
+                    messages.append(function_call_result)
+                    if verbose:
+                        print(f"-> {function_call_result.parts[0].function_response.response}")
+                except:
+                    raise Exception("Failed to match funtion to request")
+        else:
+            print(response.text)
+            break
+
+        timeout -= 1
 
 
 if __name__ == "__main__":
