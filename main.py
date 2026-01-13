613092fbefa127c53064c598b2d78218ec58ea35
Samuel Ybarra <sybarra2076@gmail.com>
2026-01-13 09:31:47 -0600

    AI agent boot.dev lesson updated

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import sys
 from google import genai
-from google.genai import types
+from google.genai import types, errors
 from dotenv import load_dotenv
 
 from prompts import system_prompt
@@ -12,11 +12,16 @@ def main():
 
     args = sys.argv[1:]
 
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
     if not args:
-        print("AI Code Assistant")
-        print('\nUsage: python main.py "your prompt here"')
-        print('Example: python main.py "How do I build a calculator app?"')
-        sys.exit(1)
+        run_token_demo(client)
+        return
+    #     print("AI Code Assistant")
+    #     print('\nUsage: python main.py "your prompt here"')
+    #     print('Example: python main.py "How do I build a calculator app?"')
+    #     sys.exit(1)
     
     verbose = False
     if "--verbose" in args:
@@ -26,10 +31,6 @@ def main():
 
     user_prompt = " ".join(args)
 
-    api_key = os.environ.get("GEMINI_API_KEY")
-    client = genai.Client(api_key=api_key)
-
-
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
@@ -37,14 +38,17 @@ def main():
     if verbose:
         print(f"User prompt: {user_prompt}")
 
-    generate_content(client, messages, verbose)
+    try:
+        generate_content(client, messages, verbose)
+    except errors.ClientError as err:
+        print(str(err))
 
 
 def generate_content(client, messages, verbose):
     timeout = 20
     while timeout != 0:
         response = client.models.generate_content(
-            model="gemini-2.0-flash-001",
+            model="gemini-2.5-flash",
             contents=messages,
             config=types.GenerateContentConfig(
                 tools=[available_functions], system_instruction=system_prompt
@@ -53,9 +57,11 @@ def generate_content(client, messages, verbose):
         for candidate in response.candidates:
             messages.append(candidate.content)
 
-        if verbose:
-            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        if response.usage_metadata is None:
+            raise RuntimeError("No usage metadata returned from Gemini API")
+
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
         if response.function_calls:
             for function_call_part in response.function_calls:
@@ -71,6 +77,20 @@ def generate_content(client, messages, verbose):
             break
 
         timeout -= 1
+
+# Fucntion for lesson update after AI agent module was completed
+def run_token_demo(client):
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+    )
+    if not response.usage_metadata:
+        raise RuntimeError("Gemini API response appears to be malformed")
+
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    print("Response:")
+    print(response.text)
 
 
 if __name__ == "__main__":
